804b6dea8e673743bfab0d835e0eb8602581e4fd
Nabdeep Shrestha <nabdeep@vt.edu>
2018-05-16 16:59:48 -0400

    Extracted method to append link for /currentDate

--- src/main/java/nubdub/com/PracticeMavenProject/controller/Controller.java
+++ src/main/java/nubdub/com/PracticeMavenProject/controller/Controller.java
@@ -18,13 +18,17 @@ public class Controller {
 	public String index() {
 		String greetings = "Greetings from Spring Boot!";
 		StringBuilder sb = new StringBuilder("<br> <a href=\"");
-		sb.append("http://localhost:8080/currentTime\">")
-			.append("Click here for current date!")
-			.append("</a>");
+		createDateLink(sb);
 		return greetings + sb.toString();
 	}
+
+	public void createDateLink(StringBuilder sb) {
+		sb.append("http://localhost:8080/currentDate\">")
+			.append("Click here for current date!")
+			.append("</a>");
+	}
 	
-	@RequestMapping("/currentTime")
+	@RequestMapping("/currentDate")
 	public String dispCurrentTime() {
 		DateTime dt = new DateTime();
 		return "The current date is " + dt.getMonthOfYear() + 
