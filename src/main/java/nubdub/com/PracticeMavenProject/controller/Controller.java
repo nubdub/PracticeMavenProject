403bf62f680b9b5aed0cd47b700103a9ce89fc2d
Nabdeep Shrestha <nabdeep@vt.edu>
2018-05-16 16:23:09 -0400

    Added method that displays year

--- src/main/java/nubdub/com/PracticeMavenProject/controller/Controller.java
+++ src/main/java/nubdub/com/PracticeMavenProject/controller/Controller.java
@@ -3,6 +3,7 @@
  */
 package nubdub.com.PracticeMavenProject.controller;
 
+import org.joda.time.DateTime;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,12 @@ public class Controller {
 	@RequestMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
+	}
+	
+	@RequestMapping("/currentTime")
+	public String dispCurrentTime() {
+		DateTime dt = new DateTime();
+		return "The current year is " + dt.getYear() + ".";
 	}
 	
 }
